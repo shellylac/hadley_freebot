3f5affa88a1d03d96feb74de16e45c25ccf8c516
Shelly Lachish <shellstar@gmail.com>
2021-10-12 11:44:13 +0100

    fixes tracking on rows tweeted

--- Code/hadley_bot_script.R
+++ Code/hadley_bot_script.R
@@ -8,23 +8,24 @@ library(rtweet)
 
 #Read in dataset
 hadley_data <- read_csv(here("Data", "hadley_dataset.csv"))
+#dataset of rows already tweeted
+rows_tweeted <- read_csv(here("Data", "rows_tweeted.csv"))
 
 #....................................
 #Function to compose the tweet
 #.....................................
 
-#indicator of dataset rows already tweeted
-row_ids_read <- 0
-
 #Function to randomly select row to tweet
 row_select <- function(dataset){
   row_id <- sample(1:dim(dataset)[1], 1)
   
-  while (row_id %in% row_ids_read) {
+  while (row_id %in% rows_tweeted) {
     row_id <- sample(1:dim(dataset)[1], 1)
   }
   #append selected row_id to row_ids_read
-  row_ids_read <<- append(row_ids_read, row_id)
+  rows_tweeted <<- rbind(rows_tweeted, row_id)
+  #Overwrite rows_tweeted dataset to update list of articles tweeted
+  write_csv(rows_tweeted, here("Data", "rows_tweeted.csv"))
   return(row_id)
 }
 
@@ -34,7 +35,7 @@ tweet_sentence <- function(dataset) {
   title <- dataset$title[row_id]
   date <- dataset$pretty_date[row_id]
   url <- dataset$url[row_id]
-  tweet <- glue("Love reading @HadleyFreeman Guardian articles? Then re-read \"{title}\" from {date}. {url}")
+  tweet <- glue("Love reading @HadleyFreeman Guardian articles? Then re-read {title} from {date}. {url}")
   return(tweet)
 }
 
@@ -43,16 +44,13 @@ tweet <- tweet_sentence(hadley_data)
 
 #Connect to Twitter
 rtweet::create_token(
-  app = "hadley_freebot",  # the name of the Twitter app
-  consumer_key = keyring::key_get("TWITTER_CONSUMER_API_KEY_bot"),
-  consumer_secret = keyring::key_get("TWITTER_CONSUMER_API_SECRET_bot"),
-  access_token = keyring::key_get("TWITTER_ACCESS_TOKEN_bot"),
-  access_secret = keyring::key_get("TWITTER_ACCESS_TOKEN_SECRET_bot")
+  app = "Hadley Freebot",  # the name of the Twitter app
+  consumer_key = keyring::key_get("TWITTER_CONSUMER_API_KEY_hadleybot"),
+  consumer_secret = keyring::key_get("TWITTER_CONSUMER_API_SECRET_hadleybot"),
+  access_token = keyring::key_get("TWITTER_ACCESS_TOKEN_hadleybot"),
+  access_secret = keyring::key_get("TWITTER_ACCESS_TOKEN_SECRET_hadleybot")
 )
-
-rtweet::post_tweet(status = "This is a test tweet.")
 
 # tweet it
 post_tweet(status = glue("{tweet}"))
-
 
